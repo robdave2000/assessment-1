0a559035b3e8176d9278daa569f4d9656a7f821e
Robert Davis <robdave22@yahoo.com>
2021-10-29 11:02:50 -0400

    sldc.js is working

--- sldc.js
+++ sldc.js
@@ -6,14 +6,21 @@
 */
 
 // Analysis of Requirements
+// Planning
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let analysisOfRequirements = "understand what the customer want the code to do, and all the critiria it has to hit";
+let planning = "write out a flowchart to get an understanding of how the requirements are going to interact with eachother";
+let design = "write a psuedocode that will give you a vauge idea of what the code will look like";
+let implementation = "code the project";
+let testingIntergration = "test the code and try to break it, if it breaks return to planning if it passes add it to what ever software it needs to be added to";
+let maintain = "keep the code up to date, this step is all of the other steps rolled into one after the code is already in the world. Plan updates like you would starting fresh code";
